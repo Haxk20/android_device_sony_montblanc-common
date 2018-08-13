b2374a928b847c0d5c5017ea397e4569c658b52a
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2018-08-14 01:24:22 +0200

    shim: separate the rest of the shims in .mk file
    Commit in Lineage15.1 changed the format of how the file should look like so change according to this change

--- board/shim.mk
+++ board/shim.mk
@@ -1,4 +1,5 @@
-export LD_SHIM_LIBS := \
+export TARGET_LD_SHIM_LIBS := \
 	/system/lib/libicuuc.so|libicuuc_51.so \
 	/system/lib/hw/camera.montblanc.so|libcamera_symbols.so \
-	suntrold|libmd5_symbols.so:/system/lib/libril.so|libril_symbols.so
+	suntrold|libmd5_symbols.so \
+	/system/lib/libril.so|libril_symbols.so
